f8fc76efd832d6cfc98f2a54cce8b5eb70489806
DoniaDoma70 <63231684+DoniaDoma70@users.noreply.github.com>
2022-07-04 15:55:33 +0200

    First Commit

---
+++ assets/js/translation.js
@@ -0,0 +1,236 @@
+const translation = {
+    en: {
+         //NavBar
+        home: "HOME",
+        about: "ABOUT",
+        service: "SERVICE",
+        projects: "PROJECTS",
+        products: "PRODUCTS",
+        blog: "BLOG",
+        faq: "FAQ",
+        contact: "CONTACT",
+        english: "English",
+        arabic: "Arabic",
+         //welcome Section
+        welcomeSpeechHeader: "WELCOME TO OUR PEAROGRAM AGENCY",
+        welcomeSpeech: "We Deliver the Software You Need",
+        ourWork: "OUR WORKS",
+         //intro Section
+        introDescriptionHeader: "PEAROGRAM THE BEST AGENCY FOR EVER",
+        introDescription: "We provide a full spectrum of IT services from software design, development, implementation and testing, to support and maintain. depending on our extensive expertise.",
+        introDescriptionPoint1: "We provide a full spectrum of IT services from software design.",
+        introDescriptionPoint2: "We provide a full spectrum of IT servicesto support and maintain.",
+        introDescriptionPoint3: "We provide a full spectrum of IT services from software design.",
+        introDescriptionPoint4: "We provide a full spectrum of IT servicesto support and maintain.",
+        purchase: "PURCHASE",
+          //feature Section
+        featureHeader1: "Responsive & Multi-purpose",
+        featureDescription: "We use the latest tools to manage each task to achieve the best performance.",
+        featureHeader2: "Clean & Clear",
+        featureDescription: "We use the latest tools to manage each task to achieve the best performance.",
+        featureHeader3: "Best UX",
+        featureDescription: "We use the latest tools to manage each task to achieve the best performance.",
+        featureHeader4: "Good Feedback",
+        featureDescription: "We use the latest tools to manage each task to achieve the best performance.",
+          //latest Projects Section"
+        latestProjectsSectionHeader: "OUR LATEST PROJECTS",
+        latestProjectsSectionSubHeader: "Absolutely Stunning Design & Functionality",
+        LPitemDescription1: "Maven Sports",
+        LPitemDescription11: "Web Development",
+        LPitemDescription2: "Tarteeb",
+        LPitemDescription21: "Web Development",
+        LPitemDescription3: "Beyond the World",
+        LPitemDescription31: "Web Development",
+         //twitter Feed Section
+        twitterFeedHead: "Have A Project In Mind? Let's Get To Work",
+        twitterFeed1: "@themewagon",
+        twitterFeed2: "2 days ago on Twitter.com",
+         //pricing Section
+        pricingHead: "AFFORDABLE PRICING",
+        pricingPara: "Absolutely Stunning Design & Functionality",
+        pricingSectionHead1: "Standard",
+        pricingSectionHead2: "Business",
+        pricingSectionHead3: "Premium",
+        pricingSectionHead4: "Ultimate",
+        pricingSpan1: "15 Projects",
+        pricingSpan2: "30 GB Storage",
+        pricingSpan3: "Ultimate Data Transfer",
+        pricingSpan4: "50 GB Bandwith",
+        pricingSpan5: "Enhanced Security",
+        pricingSpan6: "24/7 Support",
+        pricing1: "25",
+        pricing2: "35",
+        pricing3: "45",
+        pricing4: "55",
+        getStarted: "GET STARTED",
+         //Clients Section
+        clients: "CLIENTS",
+        ourClients: "Our Clients",
+        ourClientsPara:"We are proud to have successfully completed software product design.",
+         //useful Links Section
+        usefulLinks: "Useful Links",
+        usefulLink1: "Web Design",
+        usefulLink2: "About Us",
+        usefulLink3: "Mobile",
+        usefulLink4: "Blog",
+        usefulLink5: "Services",
+        usefulLink6: "Organization",
+        usefulLink7: "Career",
+        usefulLink8: "Media",
+        usefulLink9: "24/7",
+        usefulLink10: "Right Way",
+         //recent News Section
+        recentNews: "Recent News",
+        textCapitalize: "A Clear Website Gives More Experience To The Visitors",
+        newsDate: "Dec 12,2015",
+        newsetter: "E-News-Letter",
+        newsetterPara: "Sign up for our mailing list to get latest updates and offers",
+        respectPrivacy: "We respect your privacy",
+        aboutPear: "HUGE Website Builder is a big library of pre-designed web elements which help you to create website in few minutes.",
+         //nav Link Section
+        navLinkHome: "Home",
+        navLinkAbout: "About",
+        navLinkService: "Service",
+        navLinkProduct: "Product",
+        navLinkBlog: "Blog",
+        navLinkFAQ: "FAQ",
+        navLinkContact: "Contact"
+
+    },
+    ar: {
+         //NavBar
+        home: "الصفحة الرئيسية",
+        about: "من نحن",
+        service: "الخدمات",
+        projects: "المشاريع",
+        products: "الأنظمة",
+        blog: "الأخبار",
+        faq: "أسئلة مكررة",
+        contact: "تواصل معنا",
+        english: "الانجليزية",
+        arabic: "العربية",
+         //welcome Section
+        welcomeSpeechHeader: "مرحبا بك فى شركتنا PEAROGRAM",
+        welcomeSpeech: "نحن نقدم البرامج التي تحتاجها",
+        ourWork: "أعمالنا",
+         //intro Section
+         introDescriptionHeader: "PEAROGRAM أفضل وكالة على الإطلاق",
+         introDescription: "نحن نقدم مجموعة كاملة من خدمات تكنولوجيا المعلومات من تصميم البرمجيات ، والتطوير ، والتنفيذ والاختبار ، لدعم وصيانة. اعتمادًا على خبرتنا الواسعة",
+         introDescriptionPoint1: "نحن نقدم مجموعة كاملة من خدمات تكنولوجيا المعلومات من تصميم البرمجيات.",
+         introDescriptionPoint2: "نحن نقدم مجموعة كاملة من خدمات تكنولوجيا المعلومات لدعم وصيانة.",
+         introDescriptionPoint3: "نحن نقدم مجموعة كاملة من خدمات تكنولوجيا المعلومات من تصميم البرمجيات.",
+         introDescriptionPoint4: "نحن نقدم مجموعة كاملة من خدمات تكنولوجيا المعلومات لدعم وصيانة.",
+         purchase: "عملية الشراء",
+          //feature Section
+         featureHeader1: "متجاوب ومتعدد الأغراض",
+         featureDescription: "نستخدم أحدث الأدوات لإدارة كل مهمة لتحقيق أفضل أداء.",
+         featureHeader2: "نظيفة واضحة",
+         featureDescription: "نستخدم أحدث الأدوات لإدارة كل مهمة لتحقيق أفضل أداء.",
+         featureHeader3: "أفضل تجربة مستخدم",
+         featureDescription: "نستخدم أحدث الأدوات لإدارة كل مهمة لتحقيق أفضل أداء.",
+         featureHeader4: "ردود فعل طيبة",
+         featureDescription: "نستخدم أحدث الأدوات لإدارة كل مهمة لتحقيق أفضل أداء.",
+           //latest Projects Section"
+        latestProjectsSectionHeader: "أحدث مشاريعنا",
+        latestProjectsSectionSubHeader: "تصميم ووظائف مذهلة للغاية",
+        LPitemDescription1: "مافن سبورتس",
+        LPitemDescription11: "تطوير مواقع",
+        LPitemDescription2: "ترطيب",
+        LPitemDescription21: "تطوير مواقع",
+        LPitemDescription3: "Beyond the World",
+        LPitemDescription31: "تطوير مواقع",
+         //twitter Feed Section
+        twitterFeedHead: "هل لديك مشروع في الاعتبار؟ هيا بنا إلى العمل",
+        twitterFeed1: "@themewagon",
+        twitterFeed2: "قبل يومين على Twitter.com",
+         //pricing Section
+        pricingHead: "أسعار معقولة",
+        pricingPara: "تصميم ووظائف مذهلة للغاية",
+        pricingSectionHead1: "اساسي",
+        pricingSectionHead2: "اعمال",
+        pricingSectionHead3: "الممتازة",
+        pricingSectionHead4: "الذروة",
+        pricingSpan1: "15 مشروع",
+        pricingSpan2: "30 جيجا مساحة تخزين",
+        pricingSpan3: "في نهاية المطاف نقل البيانات",
+        pricingSpan4: "عرض النطاق الترددي 50 جيجا",
+        pricingSpan5: "تعزيز الأمن",
+        pricingSpan6: "24/7 دعم",
+        pricing1: "25",
+        pricing2: "35",
+        pricing3: "45",
+        pricing4: "55",
+        getStarted: "البدء",
+         //Clients Section
+        clients: "العملاء",
+        ourClients: "عملائنا",
+        ourClientsPara:"نحن فخورون بإكمال تصميم منتج البرنامج بنجاح.",
+         //useful Links Section
+        usefulLinks: "روابط مفيدة",
+        usefulLink1: "تصميم المواقع",
+        usefulLink2: "من نحن",
+        usefulLink3: "موبايل",
+        usefulLink4: "الأخبار",
+        usefulLink5: "الخدمات",
+        usefulLink6: "التنظيم",
+        usefulLink7: "مسار وظيفي",
+        usefulLink8: "وسائل الإعلام",
+        usefulLink9: "24/7",
+        usefulLink10: "طريق صحيح",
+         //recent News Section
+        recentNews: "أخبار حديثة",
+        textCapitalize: "يعطي موقع الويب الواضح المزيد من الخبرة للزوار",
+        newsDate: "12 ديسمبر 2015",
+        newsetter: "النشرة الإلكترونية",
+        newsetterPara: "اشترك في قائمتنا البريدية للحصول على آخر التحديثات والعروض",
+        respectPrivacy: "نحن نحترم خصوصيتك",
+        aboutPear: "عبارة عن مكتبة كبيرة من عناصر الويب المصممة مسبقًا والتي تساعدك على إنشاء موقع ويب في دقائق قليلة.",
+         //nav Link Section
+        navLinkHome: "الصفحة الرئيسية",
+        navLinkAbout: "من نحن",
+        navLinkService: "الخدمات",
+        navLinkProduct: "الأنظمة",
+        navLinkBlog: "الأخبار",
+        navLinkFAQ: "أسئلة مكررة",
+        navLinkContact: "تواصل معنا"
+
+    }
+
+};
+
+const languageSelector = document.querySelector("select");
+
+languageSelector.addEventListener("change", (Event) => {
+    setLanguage(Event.target.value);
+    localStorage.setItem("language", Event.target.value);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    setLanguage(localStorage.getItem("language"));
+});
+
+
+const setLanguage = (lang) => {
+    const elements = document.querySelectorAll("[data-i18n]");
+    elements.forEach((element) => {
+        const key = element.getAttribute("data-i18n");
+        element.textContent = translation[lang][key];
+    }); 
+    if (lang == "ar") {
+        document.dir = "rtl";
+        document.getElementById("headWelcomeSpeech").style.letterSpacing = "0";
+        document.getElementById("welcomeSpeechPara").style.letterSpacing = "0";
+        $("head").append('<link id="bootstrapRTL" rel="stylesheet" href="https://cdn.rtlcss.com/bootstrap/3.3.7/css/bootstrap.min.css">');
+        $("head").append('<link id="translaionCSS" rel="stylesheet" href="assets/css/translation.css">'); 
+        //$("body").append(content);
+    } else {
+        document.dir = "ltr";
+        document.getElementById("headWelcomeSpeech").style.letterSpacing = "6px";
+        document.getElementById("welcomeSpeechPara").style.letterSpacing = "3px";
+        $("#bootstrapRTL").remove();
+        $("#translaionCSS").remove();
+    }
+    //document.dir = lang === "ar" ? "rtl" : "ltr";
+};
+
+
